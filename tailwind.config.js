746e17805d1984aa2a162053afde05a058c3a0d7
Rod Adams <luckyrod72@gmail.com>
2026-02-13 20:45:12 -0800

    Downgrade to Tailwind CSS v3 for stability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,2 @@
 /** @type {import('tailwindcss').Config} */ 
-module.exports = { content: ["./app/**/*.{js,ts,jsx,tsx}"], theme: { extend: {} }, plugins: [] } 
+module.exports = { content: ['./app/**/*.{js,ts,jsx,tsx}'], theme: { extend: {} }, plugins: [] } 
